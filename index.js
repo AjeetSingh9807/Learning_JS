aaa0622a1e8e68272b283e76fed20ec78798cd3c
ajeetappics <ajeet@appicsoftwares.in>
2025-08-20 23:02:34 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 console.log('hello');
+
+// veriavals
+
+// 1: const
+const data = {
+    name: "Ajeet"
+}
+// data = { // TypeError: Assignment to constant variable.
+//     name: "Ajeet Singh"
+// }
+
+console.log(data);
+try {
+    data = { // this is try to Reassignment this variable so it through en error
+        name: "Ajeet Singh"
+    }
+    console.log(data);
+} catch (error) {
+    console.error("through an error:", error); // run
+}
+
+try {
+    const data = { //  this is a new variable with in try block
+        name: "Ajeet Singh"
+    }
+    console.log(data);
+} catch (error) {
+    console.error("through an error:", error); // run
+}
+
+// const data = {   // Cannot redeclare block-scoped variable 'data'.
+//     name: "Ajeet Singh"
+// }
+console.log(data);
